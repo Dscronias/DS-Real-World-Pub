c808664c9116161ba982a228359d9016c98a2afb
Dimitri Scronias <scronias.dimitri@gmail.com>
2024-09-16 18:49:15 +0200

    Lab 1 exercise

---
+++ Labs/main.R
@@ -0,0 +1,14 @@
+# Setup #####################
+## Libraries ################
+
+# Import all your packages you need here, at the beginning of the file
+# !Don't do it in the middle of your code!
+library(tidyverse)
+library(janitor)
+library(broom)
+
+# This tells you the "working directory" in the console (below)
+# If the working directory is not where all the .csv are, click on session (above) > Set working directory > To source file directory
+getwd()
+
+# Importing ###################
